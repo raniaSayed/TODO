c5dcee5141afff5699d232a854015992a77b51ba
raniaSayedEye <rania.sayed@eye-ltd-eg.com>
2018-11-15 02:09:15 +0200

    complete toggle complete function

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -33,7 +33,7 @@ class TodoItem extends Component {
 			<li className={classes}>
 				{this.props.task}
 				<div className="pull-right" role="group">
-					<button type="button" className="btn btn-xs btn-success img-circle" onClick={this.toggleComplete}>&#x2713;</button> <button type="button" className="btn btn-xs btn-danger img-circle" onClick={this.removeNode.bind(this)}>&#xff38;</button>
+					<button type="button" className="btn btn-xs btn-success img-circle" onClick={this.toggleComplete.bind(this)}>&#x2713;</button> <button type="button" className="btn btn-xs btn-danger img-circle" onClick={this.removeNode.bind(this)}>&#xff38;</button>
 				</div>
 			</li>
 		);
