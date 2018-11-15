161f264622cf13499d4f0176eb4ff5d7bdfabdd5
raniaSayedEye <rania.sayed@eye-ltd-eg.com>
2018-11-15 02:24:17 +0200

    add tag

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -23,8 +23,8 @@ class AddTodo extends Component {
                 todo:{
                     id: uuid.v4(),
                     task: this.refs.task.value,
-                    complete: false
-                    //tag: this.refs.tag.value
+                    complete: false,
+                    tag: this.refs.tag.value
                 }
             },() =>{
                 this.props.addTodo(this.state.todo);
@@ -37,38 +37,23 @@ class AddTodo extends Component {
 
 
     render(){
-        // let tagOptions = this.props.tags.map(tag => {
-        //     return <option key={tag} value={tag}>{tag}</option>
-        //   });
-
           return (
-            // <div>
-            //   <h3>Add Todo</h3>
-            //   <form onSubmit={this.handleSubmit.bind(this)}>
-            //     <div>
-            //       <label>Todo</label><br />
-            //       <input type="text" ref="content" />
-            //     </div>
-            //     <div>
-            //       <label>Tags</label><br />
-            //       <select ref="tag">
-            //         {tagOptions}
-            //       </select>
-            //     </div>
-            //     <br />
-            //     <input type="submit" value="Submit" />
-            //     <br />
-            //   </form>
-            // </div>
+
             <div className="commentForm vert-offset-top-2">
             <hr />
             <div className="clearfix">
                 <form className="todoForm form-horizontal" onSubmit={this.handleSubmit.bind(this)}>
                     <div className="form-group">
-                        <label htmlFor="task" className="col-md-2 control-label">Task</label>
+                        <label htmlFor="task" className="col-md-1 control-label">Task</label>
                         <div className="col-md-10">
-                            <input type="text" id="task" ref="task" className="form-control" placeholder="What do you need to do?" />
+                            <input type="text" id="task" ref="task" className="form-control" placeholder="Type Todo" />
                         </div>
+                        <hr />
+                        <label htmlFor="tag" className="col-md-1 control-label">Tag</label>
+                         <div className="col-md-5">
+                            <input type="text" id="tag" ref="tag" className="form-control" placeholder="Tag Todo" />
+                        </div>
+
                     </div>
                     <div className="row">
                         <div className="col-md-10 col-md-offset-2 text-right">
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -31,9 +31,19 @@ class TodoItem extends Component {
 		}
 		return (
 			<li className={classes}>
+			
 				{this.props.task}
+				&nbsp;&nbsp;&nbsp;&nbsp;
+
+				<button type="button" className="btn btn-xs btn-primary img-circle" 
+					onClick={this.toggleComplete.bind(this)}>{this.props.tag}</button>
+
 				<div className="pull-right" role="group">
-					<button type="button" className="btn btn-xs btn-success img-circle" onClick={this.toggleComplete.bind(this)}>&#x2713;</button> <button type="button" className="btn btn-xs btn-danger img-circle" onClick={this.removeNode.bind(this)}>&#xff38;</button>
+				
+					<button type="button" className="btn btn-xs btn-success img-circle" 
+						onClick={this.toggleComplete.bind(this)}>&#x2713;</button>
+					<button type="button" className="btn btn-xs btn-danger img-circle" 
+						onClick={this.removeNode.bind(this)}>&#xff38;</button>
 				</div>
 			</li>
 		);
